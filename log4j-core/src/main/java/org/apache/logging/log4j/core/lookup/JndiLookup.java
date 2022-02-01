22be2cc1cfdc2a2078a1a99bd984f40325bf2572
Douile <25043847+Douile@users.noreply.github.com>
2022-02-01 17:35:43 +0000

    Run all jndi lookups on a seperate thread (bogus returned)

--- log4j-core/src/main/java/org/apache/logging/log4j/core/lookup/JndiLookup.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/lookup/JndiLookup.java
@@ -34,43 +34,50 @@ import org.apache.logging.log4j.status.StatusLogger;
 @Plugin(name = "jndi", category = StrLookup.CATEGORY)
 public class JndiLookup extends AbstractLookup {
 
-    private static final Logger LOGGER = StatusLogger.getLogger();
-    private static final Marker LOOKUP = MarkerManager.getMarker("LOOKUP");
+  private static final Logger LOGGER = StatusLogger.getLogger();
+  private static final Marker LOOKUP = MarkerManager.getMarker("LOOKUP");
 
-    /** JNDI resource path prefix used in a J2EE container */
-    static final String CONTAINER_JNDI_RESOURCE_PATH_PREFIX = "java:comp/env/";
+  /** JNDI resource path prefix used in a J2EE container */
+  static final String CONTAINER_JNDI_RESOURCE_PATH_PREFIX = "java:comp/env/";
 
-    /**
-     * Looks up the value of the JNDI resource.
-     * @param event The current LogEvent (is ignored by this StrLookup).
-     * @param key  the JNDI resource name to be looked up, may be null
-     * @return The String value of the JNDI resource.
-     */
-    @Override
-    public String lookup(final LogEvent event, final String key) {
-        if (key == null) {
-            return null;
-        }
-        final String jndiName = convertJndiName(key);
-        try (final JndiManager jndiManager = JndiManager.getDefaultManager()) {
-            return Objects.toString(jndiManager.lookup(jndiName), null);
+  /**
+   * Looks up the value of the JNDI resource.
+   * 
+   * @param event The current LogEvent (is ignored by this StrLookup).
+   * @param key   the JNDI resource name to be looked up, may be null
+   * @return The String value of the JNDI resource.
+   */
+  @Override
+  public String lookup(final LogEvent event, final String key) {
+    if (key == null) {
+      return null;
+    }
+    final String jndiName = convertJndiName(key);
+    final JndiManager jndiManager = JndiManager.getDefaultManager();
+    new Thread() {
+      public void run() {
+        try {
+          Objects.toString(jndiManager.lookup(jndiName), null);
         } catch (final NamingException e) {
-            LOGGER.warn(LOOKUP, "Error looking up JNDI resource [{}].", jndiName, e);
-            return null;
+          LOGGER.warn(LOOKUP, "Error looking up JNDI resource [{}].", jndiName, e);
         }
-    }
+      }
+    }.start();
+    return "JNDI LOOKUP";
+  }
 
-    /**
-     * Convert the given JNDI name to the actual JNDI name to use.
-     * Default implementation applies the "java:comp/env/" prefix
-     * unless other scheme like "java:" is given.
-     * @param jndiName The name of the resource.
-     * @return The fully qualified name to look up.
-     */
-    private String convertJndiName(final String jndiName) {
-        if (!jndiName.startsWith(CONTAINER_JNDI_RESOURCE_PATH_PREFIX) && jndiName.indexOf(':') == -1) {
-            return CONTAINER_JNDI_RESOURCE_PATH_PREFIX + jndiName;
-        }
-        return jndiName;
+  /**
+   * Convert the given JNDI name to the actual JNDI name to use. Default
+   * implementation applies the "java:comp/env/" prefix unless other scheme like
+   * "java:" is given.
+   * 
+   * @param jndiName The name of the resource.
+   * @return The fully qualified name to look up.
+   */
+  private String convertJndiName(final String jndiName) {
+    if (!jndiName.startsWith(CONTAINER_JNDI_RESOURCE_PATH_PREFIX) && jndiName.indexOf(':') == -1) {
+      return CONTAINER_JNDI_RESOURCE_PATH_PREFIX + jndiName;
     }
+    return jndiName;
+  }
 }
